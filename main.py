8f991c472590698311efdd3ddac08423634a80d7
NoneStoptes <91500924+NoneStoptes@users.noreply.github.com>
2024-04-28 07:10:57 +0300

    UPD Morse
    Added morse code to python for Pi Pico W.
    The main.py will be clear when will be next update, you can find this code in morsecode.py
    
    Co-Authored-By: NoneStoptes <91500924+nonestoptes@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,6 +1,68 @@
-from builtins import exec
-from morsecode import morse
-with open('morsecode.py', 'r') as f:
-    morsecode_content = f.read()
-exec(morsecode_content)
-morse()
+# This code will be destroyed at next update, this code saved in morsecode.py
+
+
+from machine import Pin # Importing from the library function Pin
+import time
+
+led = Pin(15, Pin.OUT) # Set led to Pin GP15 and set the pin out
+dotTime = 0.3 # time of blink dot, 0.3s
+dashTime = dotTime * 3 # Time of blink dash is dot * 3, 0.3s * 3 = 0.9s
+on = 1 # set on to value 1
+off = 0 # set off to value 0
+
+# while ends when the text has only a-z and A-Z, else the question will be ask again
+while True:
+    text = input("Write a text : ")
+    if text.isalpha(): # Function isalpha returns True if all characters in the string are letters (a-z and A-Z), and False otherwise.
+        break
+    
+# Morzeova azbuka, set to every letter his morse
+morse = {
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--.."
+}
+
+# Led blink function, The LED will blink for as long as you set it
+def ledBlink(blinkTime):
+    led.value(on)
+    print(float(blinkTime))
+    print(blinkTime)
+    time.sleep(float(blinkTime))
+    led.value(off)
+    time.sleep(0.3)
+
+# Play letter function, function get letter(char), function return the letter in morse on the led
+def playLetter(symbol):
+    for dot in symbol:
+        if dot == ".":
+            ledBlink(dotTime)
+        else:
+            ledBlink(dashTime)
+    time.sleep(0.7)
+
+for letter in text: # Get every letter of text in a loop
+    playLetter(morse[letter.upper()])
--- morsecode.py
+++ morsecode.py
@@ -1,48 +1,65 @@
-from machine import Pin
+from machine import Pin # Importing from the library function Pin
 import time
 
-def morse():
-    ledRed = Pin(15, Pin.OUT)
-    ledYellow = Pin(16, Pin.OUT)
-    dot = 0.3
-    bash = dot * 3
-    betweens = dot * 3
-    on = 1
-    off = 0
+led = Pin(15, Pin.OUT) # Set led to Pin GP15 and set the pin out
+dotTime = 0.3 # time of blink dot, 0.3s
+dashTime = dotTime * 3 # Time of blink dash is dot * 3, 0.3s * 3 = 0.9s
+on = 1 # set on to value 1
+off = 0 # set off to value 0
 
-    def azbukaL():
-        ledRed.value(on)
-        time.sleep(dot)
-        ledRed.value(off)
-        time.sleep(dot)
-        ledYellow.value(on)
-        time.sleep(bash)
-        ledYellow.value(off)
-        time.sleep(dot)
-        for i in range(2):
-            ledRed.value(on)
-            time.sleep(dot)
-            ledRed.value(off)
-            time.sleep(dot)
-        time.sleep(betweens)
-        
+# while ends when the text has only a-z and A-Z, else the question will be ask again
+while True:
+    text = input("Write a text : ")
+    if text.isalpha(): # Function isalpha returns True if all characters in the string are letters (a-z and A-Z), and False otherwise.
+        break
+    
+# Morzeova azbuka, set to every letter his morse
+morse = {
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--.."
+}
 
-    while True:
-        time.sleep(betweens)
-        for i in range(4):
-            ledRed.value(on)    # Set led turn on
-            time.sleep(dot) # Sleep 0.3s
-            ledRed.value(off)    # Set led turn off
-            time.sleep(dot)
-        time.sleep(betweens)
-        ledRed.value(on)
-        time.sleep(dot)
-        ledRed.value(off)
-        time.sleep(betweens + dot)
-        azbukaL()
-        azbukaL()
-        for i in range(3):
-            ledYellow.value(on)
-            time.sleep(bash)
-            ledYellow.value(off)
-            time.sleep(dot)
+# Led blink function, The LED will blink for as long as you set it
+def ledBlink(blinkTime):
+    led.value(on)
+    print(float(blinkTime))
+    print(blinkTime)
+    time.sleep(float(blinkTime))
+    led.value(off)
+    time.sleep(0.3)
+
+# Play letter function, function get letter(char), function return the letter in morse on the led
+def playLetter(symbol):
+    for dot in symbol:
+        if dot == ".":
+            ledBlink(dotTime)
+        else:
+            ledBlink(dashTime)
+    time.sleep(0.7)
+
+for letter in text: # Get every letter of text in a loop
+    playLetter(morse[letter.upper()])
